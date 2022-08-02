fef4b7aee1b2161d940eb2c4be57e98f10a86575
bignyokki <shiro5989@yahoo.co.jp>
2022-08-02 17:57:05 +0900

    算出プロパティ

--- app.js
+++ app.js
@@ -29,5 +29,23 @@ var vm = new Vue({
       }
       return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,')
     }
+  },
+  computed: { // 算出プロパティ
+    totalPrice: function () {
+      // this経由でインスタンス内のデータにアクセス
+      // reduceメソッド: 配列の要素を一つずつ処理して結果を返す
+      //   array.reduce( function( 引数1, 引数2, 引数3 引数4 ){return 処理}, 初期値 )
+      //     引数1: 処理前の値（直前の処理結果）(sum)
+      //     引数2: 現在の要素の値(item)
+      //     引数3: 現在の要素のインデックス番号(なし)
+      //     引数4: 元の配列(なし)
+      return this.items.reduce(function (sum, item) {
+        return sum + (item.price * item.quantity)
+      }, 0)
+    },
+    totalPriceWithTax: function () {
+      // 算出プロパティに依存した算出プロパティーも定義できる
+      return Math.floor(this.totalPrice * 1.08)
+    }
   }
 })
